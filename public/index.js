72bb70802aafc4158226d6d34fa6e8f0f2a2461d
MaximoGismondi <maximogismondi@gmail.com>
2023-01-27 17:29:02 -0300

    Avances en las dimensiones

--- public/index.js
+++ public/index.js
@@ -1,61 +1,94 @@
-LETRAS = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+NOMENCLATURA_CASILLEROS = [
+  [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ],
+  [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+  ],
+  [
+    "α",
+    "β",
+    "γ",
+    "δ",
+    "ε",
+    "ζ",
+    "η",
+    "θ",
+    "ι",
+    "κ",
+    "λ",
+    "μ",
+    "ν",
+    "ξ",
+    "ο",
+    "π",
+    "ρ",
+    "σ",
+    "τ",
+    "υ",
+    "φ",
+    "χ",
+    "ψ",
+    "ω",
+  ],
 ];
 
-NUMEROS = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-];
+// ["α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι", "κ", "λ", "μ", "ν", "ξ", "ο", "π", "ρ", "σ", "τ", "υ", "φ", "χ", "ψ", "ω"]
+// ['Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω']
+// ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"]
 
 COLORES = ["W", "B"];
 
@@ -73,6 +106,7 @@ NOMENCLATURA_SEPARADOR = "-";
 NOMENCLATURA_INFINITO = "∞";
 NOMENCLATURA_EXCLUSIVO = "*";
 
+REGLAS_JUEGO_DEFECTO = {};
 REGLAS_MOVIMIENTO_DEFECTO = {
   forward_only: false,
   capture: true,
@@ -87,26 +121,45 @@ REGLAS_PIEZA_DEFECTO = {
 };
 
 class Juego {
-  constructor(largo, alto) {
-    this.tablero = [];
-    LETRAS.slice(0, largo).forEach((letra) => {
-      this.tablero.push([]);
-      NUMEROS.slice(0, alto).forEach((numero) => {
-        this.tablero[this.tablero.length - 1] = new Casillero(letra, numero);
-      });
-    });
+  constructor(casilleros_dimension) {
+    this.reglas = REGLAS_JUEGO_DEFECTO;
+    this.dimension = casilleros_dimension.length;
+    this.tablero = new Map();
+    this._crear_tablero([], casilleros_dimension);
   }
 
-  agregar_pieza(letra, numero, pieza) {
+  _crear_tablero(nomenclatura_acumulada, casilleros_dimension) {
+    let dimension_actual = nomenclatura_acumulada.length;
+    let dimension_total = casilleros_dimension.length;
+
+    if (dimension_actual == dimension_total - 1) {
+      for (let i = 0; i < casilleros_dimension[dimension_actual]; i++) {
+        this.tablero.set(
+          nomenclatura_acumulada.concat(i).join(NOMENCLATURA_SEPARADOR),
+          new Casillero(nomenclatura_acumulada.concat(i))
+        );
+      }
+      return;
+    }
+
+    for (let i = 0; i < casilleros_dimension[dimension_actual]; i++) {
+      this._crear_tablero(
+        nomenclatura_acumulada.concat(i),
+        casilleros_dimension
+      );
+    }
+  }
+
+  agregar_pieza(posicion, pieza) {
     //el casillero debe estar vacio?
     //no puede haber 2 piezas con la misma nomenclatura y color
-    this.tablero[LETRAS.indexOf(letra)][
-      [NUMEROS.indexOf(numero)]
-    ].actualizar_contenido(pieza);
+    this.tablero
+      .get(posicion.join(NOMENCLATURA_SEPARADOR))
+      .actualizar_contenido(pieza);
   }
 
   obtener_casillero(letra, numero) {
-    return this.tablero[LETRAS.indexOf(letra)][NUMEROS.indexOf(numero)];
+    return this.tablero[letra][numero];
   }
 
   obtener_pieza(letra, numero) {
@@ -118,14 +171,14 @@ class Juego {
 }
 
 class Casillero {
-  constructor(letra, numero, contenido = null) {
-    this.letra = letra;
-    this.numero = numero;
+  constructor(posicion, contenido = null) {
+    this.posicion = posicion;
+    this.dimension = posicion.length;
     this.contenido = contenido;
   }
 
   nomenclatura() {
-    return letra + this.numero;
+    return this.posicion.join(NOMENCLATURA_CONECTOR);
   }
   esta_vacio() {
     return this.contenido == null;
@@ -138,14 +191,7 @@ class Casillero {
 class Movimiento {
   constructor(direcciones, reglas = {}) {
     this.direcciones = direcciones;
-
-    REGLAS_MOVIMIENTO_DEFECTO.forEach((value, key) => {
-      if (!reglas.has(key)) {
-        reglas[key] = value;
-      }
-    });
-
-    this.reglas = reglas;
+    this.reglas = Object.assign({ ...REGLAS_MOVIMIENTO_DEFECTO }, reglas);
   }
   nomenclatura() {
     return this.direcciones
@@ -171,12 +217,18 @@ class Paso {
 }
 
 class Pieza {
-  constructor(color, nombre, numero) {
+  constructor(
+    color,
+    nombre,
+    numero,
+    posibles_movimientos = [],
+    reglas = { ...REGLAS_PIEZA_DEFECTO }
+  ) {
     this.color = color;
     this.numero = numero;
     this.nombre = nombre;
-    this.posibles_movimientos = [];
-    this.reglas = REGLAS_PIEZA_DEFECTO;
+    this.posibles_movimientos = posibles_movimientos;
+    this.reglas = reglas;
   }
   nomenclatura() {
     return [color, nombre, numero].join(NOMENCLATURA_SEPARADOR);
@@ -190,7 +242,10 @@ class Pieza {
 
 class Rey extends Pieza {
   constructor(color, numero) {
-    super(color, NOMENCLATURA_REY, numero);
+    super(color, NOMENCLATURA_REY, numero, {
+      check: true,
+      castling: true,
+    });
     this.posibles_movimientos = [
       new Movimiento([new Paso(DIRECCIONES[0], 1)]),
       new Movimiento([new Paso(DIRECCIONES[1], 1)]),
@@ -201,8 +256,10 @@ class Rey extends Pieza {
       new Movimiento([new Paso(DIRECCIONES[6], 1)]),
       new Movimiento([new Paso(DIRECCIONES[7], 1)]),
     ];
-    this.reglas["check"] = true;
-    this.reglas["castling"] = true;
+    Object.assign(this.reglas, {
+      check: true,
+      castling: true,
+    });
   }
 }
 
@@ -231,7 +288,9 @@ class Torre extends Pieza {
       new Movimiento([new Paso(DIRECCIONES[2])]),
       new Movimiento([new Paso(DIRECCIONES[3])]),
     ];
-    this.reglas["castling"] = true;
+    Object.assign(this.reglas, {
+      castling: true,
+    });
   }
 }
 
@@ -284,7 +343,9 @@ class Caballo extends Pieza {
         { exact: true }
       ),
     ];
-    this.reglas["jump"] = true;
+    Object.assign(this.reglas, {
+      jump: true,
+    });
   }
 }
 
@@ -327,10 +388,18 @@ class Peon extends Pieza {
         capture_only: true,
       }),
     ];
-    this.reglas = { promotion: true };
+    Object.assign(this.reglas, {
+      promotion: true,
+    });
   }
 }
 
-let juego = new Juego(8, 8);
-juego.agregar_pieza("a", "1", new Rey(COLORES[0], 1));
-console.log(juego.obtener_pieza("a", "1").obtener_posibles_movimientos());
+console.log("numero: le pondria otro nombre a eso");
+
+TAMANO = [8, 8];
+
+let juego = new Juego(TAMANO);
+juego.agregar_pieza([0, 0], new Rey(0, 1));
+juego.agregar_pieza([0, 1], new Caballo(0, 1));
+console.log(juego);
+// console.log(juego.obtener_pieza(0, 0).obtener_posibles_movimientos());
